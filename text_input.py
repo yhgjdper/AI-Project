42e19b5ca264725751ea5cf99cf986c6940da3ed
Reas Aser <yhgj7139312580@gmail.com>
2020-07-24 13:50:17 -0400

    begin closefile

--- text_input.py
+++ text_input.py
@@ -1,7 +1,13 @@
 import subprocess
 command = input("text\n")
 if command[:5] == "open ":
-    command1 = command[6:]
+    command1 = command[5:]
     process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
+elif command[:6] == "close ":
+    command1 = command[6:]
+    print(command1)
+    print('/IM ' + command1 + ' /F')
+    process = subprocess.Popen(['taskkill', '/im ' + command1 + '.exe'], stdout=subprocess.PIPE, universal_newlines=True)
 else:
-    print("no")
+    print('no')
+    
