f7d1fdb31c76a451cbcb80eb87325ca2465edf52
Joaquin Rey <joaquinrey2004@gmail.com>
2020-07-24 16:53:38 -0400

    Worked on express

--- Website/index.js
+++ Website/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('<h1> HEHEHEHE</h1>'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.post('/settings', (req, res) => {
+    //Sends back setting variables
+
+
+    
+    res.send(200);
+});
+
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
