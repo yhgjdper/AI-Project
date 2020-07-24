05584925c9aa3e983d099ae19468ce8bc26dc8d1
Reas Aser <yhgj7139312580@gmail.com>
2020-07-23 21:28:48 -0400

    hotfix

--- text_input.py
+++ text_input.py
@@ -2,6 +2,6 @@ import subprocess
 command = input("text\n")
 if command[:5] == "open ":
     command1 = command[6:]
-    process = subprocess.Popen(['explorer', 'command1'], stdout=subprocess.PIPE, universal_newlines=True)
+    process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
 else:
     print("no")
