4dcd126ea7f19381183d4e561be76fdb1a3c0fca
Joaquin Rey <joaquinrey2004@gmail.com>
2020-07-24 15:28:57 -0400

    Fixed a typo

--- Website/index.js
+++ Website/index.js
@@ -1,7 +1,7 @@
-let app = require('express');
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('<h1> BEEEG GAMERS GAMING </h>');
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, console.log('Server listening in 3000'));
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
