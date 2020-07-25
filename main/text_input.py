ae948d2852e278e09de6b7d6c786f44eb405a3e2
Reas Aser <yhgj7139312580@gmail.com>
2020-07-25 15:35:08 -0400

    google stuff

--- main/text_input.py
+++ main/text_input.py
@@ -1,13 +1,21 @@
 import subprocess
 import os
+import pyautogui
+import webbrowser
+import time
+
 def tinput(command):
-    if command[:5] == "open ":
+    if command[:5] == "open " or command[:5] == "Open ":
         command1 = command[5:]
         process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
-    elif command[:6] == "close ":
+    elif command[:6] == "close " or command[:6] == "Close ":
         command1 = command[6:]
         os.system(f"TASKKILL /F /IM {command1}.exe")
-    else:
-        print('no')
+    elif command[:7] == "google " or command[:7] == "Google ":
+        command1 = command[7:]
 
-tinput(command = input("text\n"))
+        webbrowser.open('https://google.com/?q=' + command1)
+        time.sleep(1)
+        pyautogui.press('enter')
+
+tinput(command = input("What do you want to do?\n"))
