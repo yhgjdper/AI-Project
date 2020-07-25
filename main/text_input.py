554dd91918ab5c7c8985ae9020964d986c4c9613
Reas Aser <yhgj7139312580@gmail.com>
2020-07-25 14:10:46 -0400

    change to func

--- main/text_input.py
+++ main/text_input.py
@@ -1,13 +1,14 @@
 import subprocess
-command = input("text\n")
-if command[:5] == "open ":
-    command1 = command[5:]
-    process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
-elif command[:6] == "close ":
-    command1 = command[6:]
-    print(command1)
-    print('/IM ' + command1 + ' /F')
-    process = subprocess.Popen(['taskkill', '/im ' + command1 + '.exe'], stdout=subprocess.PIPE, universal_newlines=True)
-else:
-    print('no')
-    
+def tinput(command):
+    if command[:5] == "open ":
+        command1 = command[5:]
+        process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
+    elif command[:6] == "close ":
+        command1 = command[6:]
+        print(command1)
+        print('/IM ' + command1 + '.exe')
+        process = subprocess.Popen(['taskkill', '/im ' + command1 + '.exe'], stdout=subprocess.PIPE, universal_newlines=True)
+    else:
+        print('no')
+
+tinput(command = input("text\n"))
