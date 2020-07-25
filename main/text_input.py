8bd9bb640df95c0cee497bd1c9682785ba416a74
Reas Aser <yhgj7139312580@gmail.com>
2020-07-25 14:43:28 -0400

    close file fixd

--- main/text_input.py
+++ main/text_input.py
@@ -1,13 +1,12 @@
 import subprocess
+import os
 def tinput(command):
     if command[:5] == "open ":
         command1 = command[5:]
         process = subprocess.Popen(['explorer', command1], stdout=subprocess.PIPE, universal_newlines=True)
     elif command[:6] == "close ":
         command1 = command[6:]
-        print(command1)
-        print('/IM ' + command1 + '.exe')
-        process = subprocess.Popen(['taskkill', '/im ' + command1 + '.exe'], stdout=subprocess.PIPE, universal_newlines=True)
+        os.system(f"TASKKILL /F /IM {command1}.exe")
     else:
         print('no')
 
